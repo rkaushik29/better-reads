2b04411bb142b104cc793185dd2c55e8a6f5d11c
Rohit Kaushik <rohit.rk.rk1@gmail.com>
2025-02-01 13:00:34 -0500

    push key

--- src/env.js
+++ src/env.js
@@ -12,6 +12,7 @@ export const env = createEnv({
       .enum(["development", "test", "production"])
       .default("development"),
     GOOGLE_BOOKS_API_KEY: z.string(),
+    OPENAI_API_KEY: z.string(),
   },
 
   /**
@@ -31,6 +32,7 @@ export const env = createEnv({
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
     GOOGLE_BOOKS_API_KEY: process.env.GOOGLE_BOOKS_API_KEY,
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
   /**
