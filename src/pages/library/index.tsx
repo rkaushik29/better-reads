5ead0ca0b3b103a011c0c6f3c23a32b1aac0018d
Rohit Kaushik <rohit.rk.rk1@gmail.com>
2025-02-01 18:25:38 -0500

    works

--- src/pages/library/index.tsx
+++ src/pages/library/index.tsx
@@ -1,10 +1,13 @@
-import { BookSearchWithTag } from "@/components/BookSearchWithTag";
+import { Button } from "@/components/ui/button";
 import React from "react";
 
 export default function Library() {
   return (
     <div>
-      <BookSearchWithTag />
+      <div className="flex justify-between items-center">
+        <h1 className="text-2xl font-bold">Library</h1>
+        <Button>Add Book</Button>
+      </div>
     </div>
   );
 }
