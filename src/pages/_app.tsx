9b0cea336d10d00af7d69d2d92f8f6650758e24c
Rohit Kaushik <rohit.rk.rk1@gmail.com>
2025-02-01 14:53:03 -0500

    changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,14 @@ import type { AppProps } from "next/app";
 import { httpBatchLink } from "@trpc/client";
 import { createTRPCNext } from "@trpc/next";
 import type { AppRouter } from "@/server/routers/_app";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from "@clerk/nextjs";
+import "@/styles/globals.css";
 
 const trpc = createTRPCNext<AppRouter>({
   config() {
@@ -17,7 +25,17 @@ const trpc = createTRPCNext<AppRouter>({
 });
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ClerkProvider>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
 }
 
 export default trpc.withTRPC(App);
